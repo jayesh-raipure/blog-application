385af927c345400ada793acfec7edf906acede04
jayesh-raipure <jayesh.raipure1@gmail.com>
2017-07-13 15:36:41 +0530

    Styling a app

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,13 +8,47 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+  
+  def update
+    @post = Post.find(params[:id])
+    respond_to do |format|
+      if @post.update(post_params)
+        format.html { redirect_to @post, notice: "Blog Updated Successfully." }
+        format.json { render :show, status: :created, location: @post }
+      else
+        format.html { render :edit }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def create 
     @post = Post.new(post_params)
-    @post.save
-
-    redirect_to @post
+    
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to @post,  notice: "Blog Created Successfully." }
+        format.json { render :show, status: :created, location: @post }
+      else
+        format.html { render :new }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    respond_to do |format|
+      format.html { redirect_to posts_url, notice: 'Blog was successfully Deleted.' }
+      format.json { head :no_content }
+    end
   end
 
   private
